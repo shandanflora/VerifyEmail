a84ccce302969bd21a0ebdbe193cedbb51db184e
lily.shan <shandan.flora@gmail.com>
2016-12-16 09:58:53 +0800

    commit for adding screenshot and increase delay time

--- src/main/java/com/ecovacs/test/common/Common.java
+++ src/main/java/com/ecovacs/test/common/Common.java
@@ -1,9 +1,11 @@
 package com.ecovacs.test.common;
 
-import org.openqa.selenium.NoSuchElementException;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
+import org.openqa.selenium.remote.Augmenter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.io.File;
 
 /**
  * Created by ecosqa on 16/12/6.
@@ -36,7 +38,7 @@ public class Common {
         int iLoop = 0;
         boolean bResult = false;
         while (true){
-            if(iLoop > 100){
+            if(iLoop > 150){
                 break;
             }
             try {
@@ -44,12 +46,51 @@ public class Common {
                 bResult = true;
                 break;
             }catch (NoSuchElementException e){
-                logger.info("load web html again!!!");
+                logger.info("load web html again-" + String.valueOf(iLoop));
                 Common.getInstance().waitSecond(500);
             }
             iLoop++;
         }
         return bResult;
+    }
+
+    private boolean delAllFile(String path) {
+        File file = new File(path);
+        File temp;
+        String[] tempList = file.list();
+        if(tempList == null){
+            return false;
+        }
+        for(String strFile:tempList){
+            if (path.endsWith(File.separator)) {
+                temp = new File(path + strFile);
+            } else {
+                temp = new File(path + File.separator + strFile);
+            }
+            if (temp.isFile()) {
+                if(!temp.delete()){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public boolean screenShot(String strFileName, WebDriver driver){
+        TakesScreenshot screen = (TakesScreenshot ) new Augmenter().augment(driver);
+        String strPath = getClass().getResource("/").getPath()
+                + "../" + "screenShots/";
+        //check
+        File folder = new File(strPath);
+        if(!folder.exists() && !folder.isDirectory()){
+            if(!folder.mkdir()){
+                return false;
+            }
+        }else {
+            delAllFile(strPath);
+        }
+        File ss = new File(strPath + strFileName);
+        return screen.getScreenshotAs(OutputType.FILE).renameTo(ss);
     }
 
 }
